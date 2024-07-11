51904f9c47a3bcf42ed75fbad1669b4af7d259e3
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-07-11 02:40:36 +0100

    user model, auth route  and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 const PORT = 8000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({extended: true})); //to parse form data
+app.use(cookieParser());
+
 //routes
 app.use("/api/auth", authRoutes)
 
+
+//testing route
 app.get("/", (req, res) => {
     res.send("Yo!")
 });
