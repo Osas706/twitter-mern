f554e4ea14585f9614aa61ed736257db92bd45ef
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-07-11 20:35:23 +0100

    user api routes nd controllers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes.js'
-import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
+
+import connectMongoDB from './db/connectMongoDB.js';
+
 
 dotenv.config();
 const app = express();
@@ -14,6 +18,7 @@ app.use(cookieParser());
 
 //routes
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 
 //testing route
