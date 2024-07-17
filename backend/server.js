f8f98c446a688810905bd641649799cb22a67c52
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-07-17 21:12:38 +0100

    createPost func added

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,10 @@ cloudinary.config({
 const app = express();
 const PORT = 8000;
 
-app.use(express.json()); // to parse req.body
-app.use(express.urlencoded({extended: true})); //to parse form data
+app.use(express.json({limit: '50mb'})); // to parse req.body
+app.use(express.urlencoded({extended: true, limit: '50mb'})); //to parse form data
 app.use(cookieParser());
+// app.use(express.bodyParser({limit: '50mb'}));
 
 //routes
 app.use("/api/auth", authRoutes)
