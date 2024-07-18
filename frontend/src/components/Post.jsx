31b5058c882365b5e7a541d7756cd28f3c1dffaa
Osas706 <92319443+Osas706@users.noreply.github.com>
2024-07-18 22:00:28 +0100

    commentOnPost added

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -10,11 +10,16 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {toast} from 'react-hot-toast';
 
 import LoadingSpinner from '../components/LoadingSpinner';
+import { formatPostDate } from "../utils/formatDate";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
   const {data: authUser} = useQuery({queryKey: ["authUser"]});
   const queryClient = useQueryClient();
+  const postOwner = post.user;
+  const isLiked = post.likes.includes(authUser._id);
+  const isMyPost = authUser._id === post.user._id;
+  const formattedDate = formatPostDate(post.createdAt);
 
   //deletePost func/mutation
   const {mutate: deletePost, isPending: isDeleting} = useMutation({
@@ -70,7 +75,7 @@ const Post = ({ post }) => {
         if (p._id === post._id) {
           return { ...p, likes: updatedLikes };
         }
-        
+
         return p;
       });
     })
@@ -81,15 +86,38 @@ const Post = ({ post }) => {
    }
   });
 
+  //comentPost func/mutation
+  const {mutate: commentPost, isPending: isCommenting} = useMutation({
+    mutationFn: async() => {
+      try {
+        const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment }),
+        })
+        const data = await res.json();
+  
+        if(!res.ok){
+          throw new Error(data.error || "Somthing went wrong");
+        };
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
 
-  const postOwner = post.user;
-  const isLiked = post.likes.includes(authUser._id);
+    onSuccess: () => {
+			// toast.success("Comment posted successfully");
+			setComment("");
+			queryClient.invalidateQueries({ queryKey: ["posts"] });
+		},
 
-  const isMyPost = authUser._id === post.user._id;
-
-  const formattedDate = "1h";
-
-  const isCommenting = false;
+		onError: (error) => {
+			toast.error(error.message);
+		},
+  });
 
   const handleDeletePost = () => {
     deletePost();
@@ -97,6 +125,8 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if(isCommenting) return;
+    commentPost();
   };
 
   const handleLikePost = () => {
